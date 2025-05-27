4ab470ef358b2f15a151b898bbd7f0eeaf97446e
vfmjoao <joaovictorfigueiredomedeiros@gmail.com>
2025-05-26 21:04:50 -0400

    Implementado o scroll na tela de Adicionar Tarefa

--- src/screens/NovaTarefa.js
+++ src/screens/NovaTarefa.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
@@ -39,11 +39,11 @@ export default function NovaTarefa() {
     }
 
     return (
-        <View>
+        <View style={styles.container}>
             <View style={styles.cabecalho}>
                 <Text style={styles.titulo}>Adicionar Tarefa</Text>
             </View>
-            <View style={styles.body}>
+            <ScrollView style={styles.body}>
                 <Text style={styles.texto}>Nome da Tarefa:</Text>
                 <TextInput style={styles.textInput} value={nome} onChangeText={texto => setNome(texto)} />
 
@@ -84,7 +84,7 @@ export default function NovaTarefa() {
                     </TouchableOpacity>
                 </View>
 
-            </View>
+            </ScrollView>
         </View>
     )
 }
@@ -109,7 +109,7 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
     body: {
-        padding: 15,
+        padding: 15
     },
     texto: {
         marginBottom: 5
